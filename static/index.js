2d32dd9328ea7ca5b22234115ae4bd54a02e292c
Daniel Diniz <daniel.diniz1@icloud.com>
2021-07-06 22:02:14 -0300

    ajuste JS

--- static/index.js
+++ static/index.js
@@ -1,23 +1,32 @@
 function validar() {
-    var nomeanimal = document.getElementById("nomeanimal")
-    var nomecientifico = document.getElementById("nomecientifico")
-    var imagemurl = document.getElementById("imagemurl")
+    var nomeanimal = document.getElementById("nomeanimal");
+    var nomecientifico = document.getElementById("nomecientifico");
+    var imagemurl = document.getElementById("imagemurl");
+    var nivelperigo = document.getElementById("nivelperigo");
 
     if (nomeanimal.value == "") { // verifica se o campo NÃO foi preenchido //
         alert("Nome do animal não informado"); // se verdadeiro, emite um alerta //
         nomeanimal.focus();  //deixa o input com o focus //
-        return; // retorna a função e não olha as outras linhas
+        return false; // retorna a função e não olha as outras linhas
     }
 
-    if (nomecientifico.value == "") { // verifica se o campo NÃO foi preenchido //
-        alert("Nome científico do animal não informado"); // se verdadeiro, emite um alerta //
-        nomecientifico.focus(); // deixa o input com o focus //
-        return; // retorna a função e não olha as outras linhas
+    if (nomecientifico.value == "") { 
+        alert("Nome científico do animal não informado"); 
+        nomecientifico.focus(); 
+        return false; 
     }
 
-    if (imagemurl.value == "") { // verifica se o campo NÃO foi preenchido //
-        alert("URL da imagem não foi informado"); // se verdadeiro, emite um alerta //
-        imagemurl.focus(); // deixa o input com o focus //
-        return; // retorna a função e não olha as outras linhas
+    if (imagemurl.value == "") { 
+        alert("URL da imagem não foi informado"); 
+        imagemurl.focus(); 
+        return false; 
     }
+
+    if (nivelperigo.value == "") { 
+        alert("O nível de perigo não foi informado"); 
+        nivelperigo.focus(); 
+        return false; 
+    }
+
+
 }
